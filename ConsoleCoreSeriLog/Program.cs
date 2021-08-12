8f78359ba4cd3e4fb804775b3318820822d3f4fb
ruiapmoraes <rui-moraes@live.com>
2021-08-11 23:37:35 -0300

    Add project files.

---
+++ ConsoleCoreSeriLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+
+namespace ConsoleCoreSeriLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Information("Usando o Serilog...");
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            Log.Information("Usando o Serilog");
+            Console.ReadKey();
+        }
+    }
+}
