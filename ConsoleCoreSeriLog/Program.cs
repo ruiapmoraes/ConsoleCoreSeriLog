d2f846df5ae82055a41293f7fb80712ed2de4fb6
ruiapmoraes <rui-moraes@live.com>
2021-08-11 23:45:06 -0300

    Mais Testes

--- ConsoleCoreSeriLog/Program.cs
+++ ConsoleCoreSeriLog/Program.cs
@@ -17,7 +17,30 @@ namespace ConsoleCoreSeriLog
                 .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
             Log.Information("Usando o Serilog");
+            //Console.ReadKey();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\meuLoggerApp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Olá Serilog");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug($"Dividindo {a} por {b}.");
+                Console.WriteLine(a/b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Alguma coisa deu errado...");
+            }
+
+            Log.CloseAndFlush();
             Console.ReadKey();
+                
         }
     }
 }
